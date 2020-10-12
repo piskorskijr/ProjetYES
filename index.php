25e5fb220e5a1e1134fd1bd77679bd3ad8f3ed41
Loan PISKORSKI <49648384+piskorskijr@users.noreply.github.com>
2020-10-12 17:26:07 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
   <meta charset="utf-8" name="viewport" content="device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style3.css">
 </head>
+<meta http-equiv="refresh" content="10">
 <body>
   <div class="wrapper">
     <div class="one">
@@ -36,7 +37,7 @@
       <div class="numbers"></div>
     </div>
     <div class="five">
-      <div class="text">Informations</div>
+      <div class="text">Alertes Mairie</div>
       <div class="numbers"></div>
     </div>
   </div>
@@ -44,8 +45,8 @@
 
 <script type="text/javascript">
   setTimeout(function(){
-    window.location.reload(15);
-  }, 90000);
+    window.location.reload(10);
+  }, 30000);
   var couleur= "<?php echo $data['COULEUR_AIR'];?>";
   document.getElementById('toto').style.backgroundColor = couleur;
 </script>
